b115d9d1242c669d1b45cb5e1f112246eae83282
ghb <1184976389@qq.com>
2021-11-13 20:51:24 +0800

    init

---
+++ app/src/main/java/com/example/breast_symmetry_evaluation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.breast_symmetry_evaluation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
